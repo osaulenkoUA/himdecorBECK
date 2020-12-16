cf66357b37929021f5e578c7f0f94d7756af700c
corsairsss <oom2212@gmail.com>
2020-12-16 16:35:42 +0200

    qwerty

--- api/products/products.model.js
+++ api/products/products.model.js
@@ -2,17 +2,21 @@ const mongoose = require('mongoose');
 const { Schema } = mongoose;
 
 const productSchema = new Schema({
+  group: { type: String, required: true },
   name: { type: String, required: true },
   fasovka: { type: Array, required: false },
   sklad: { type: String, required: false },
   time: { type: String, required: false },
   vutratu: { type: String, required: false },
   solvent: { type: String, required: false },
-  vudu_robit: { type: String, required: false },
+  vudurobit: { type: String, required: false },
   vlastuvosti: { type: String, required: false },
   pidgotovka: { type: String, required: false },
   nanesennya: { type: String, required: false },
   urlimage: { type: String, required: false },
+  buyurl: { type: String, required: false },
+  matchurl: { type: String, required: false },
+
 });
 
 const productModel = mongoose.model('Product', productSchema);
